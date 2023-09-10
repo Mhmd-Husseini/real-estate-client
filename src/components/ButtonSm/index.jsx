b0915cb8b85d225ac9dd31234a969d720f525508
Husseini <mhmd.husseinyy@gmail.com>
2023-09-10 17:45:47 +0300

    creating LoginForm component

--- src/components/ButtonSm/index.jsx
+++ src/components/ButtonSm/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-const ButtonSm = ({ buttonText }) => {
+const ButtonSm = ({ buttonText, onClick }) => {
   return (
-    <button className="text-neutral-800 py-4 font-semibold px-5 rounded-tr-2xl bg-primary hover:bg-secondary hover:text-white text-sm whitespace-pre-wrap">
+    <button onClick={() => onClick()} className="text-neutral-800 py-4 font-semibold px-5 rounded-tr-2xl bg-primary hover:bg-secondary hover:text-white text-sm whitespace-pre-wrap">
         {buttonText}   <FontAwesomeIcon icon={faArrowRight} />
     </button>
   )
---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Input from "../Input";
+import ButtonSm from "../ButtonSm";
+import { sendRequest } from "../../config/request";
+import { useNavigate } from "react-router-dom";
+
+const LoginForm = ({ onToggle }) => {
+  const navigation = useNavigate();
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const loginHandler = async () => {
+    if (!validateEmail(credentials.email)) {
+      setError("Invalid Email Address");
+      return;
+    }
+
+    if (credentials.password.length < 8) {
+      setError("Password must be at least 8 characters long");
+      return;
+    }
+
+    try {
+      const response = await sendRequest({ method: "POST", route: "guest/login", body: credentials });
+
+      localStorage.setItem("access_token", response.token);
+      navigation("/");
+    } catch (error) {
+      console.log(error);
+      setError("Incorrect Email/Password");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-secondary">
+          Lebanon RealEstate Insights
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={(e) => e.preventDefault()}>
+          <Input label="Email" placeholder="Enter your email" type="email" value={credentials.email} onChange={(email) => setCredentials({ ...credentials, email })}/>
+          <Input label="Password" placeholder="Enter your password" type="password" value={credentials.password} onChange={(password) => setCredentials({ ...credentials, password })}/>
+          {error && <p className="text-red-500 text-xs">{error}</p>}
+          <div className="flex justify-center">
+            <ButtonSm onClick={loginHandler} buttonText="Login"/>
+          </div>
+          <p className=" text-center text-gray-600">
+          Don't have an account?{" "}
+          <span className="cursor-pointer text-secondary font-semibold" onClick={() => onToggle()}>
+            Register here
+          </span>
+        </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
