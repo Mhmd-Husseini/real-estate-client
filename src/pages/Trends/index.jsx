e5b8029eedb052648fd8d0b5e36b99b4dfa75abb
Husseini <mhmd.husseinyy@gmail.com>
2023-09-13 15:18:06 +0300

    styling trends page

--- src/pages/Trends/index.jsx
+++ src/pages/Trends/index.jsx
@@ -49,18 +49,18 @@ const Trends = () => {
           Get Insights into the Real Estate Market
         </p>
       </div>
-      <div className="max-w-screen-lg mx-auto border-4 border-primary p-8">
+      <div className="max-w-screen-lg mx-auto border-4 border-primary p-8 mb-32">
         <canvas id="myChart" width={400} height={200}></canvas>
       </div>
       <div className="article-list mt-8">
         <ul>
           {data.articles.map((article) => (
-            <li key={article.id}>
+            <li className='mb-16' key={article.id}>
               <h1 className='font-bold text-3xl mb-2 text-gray-800'>{article.title}</h1>
               <div className='h-1 w-28 bg-gradient-to-r from-primary to-black mb-6'></div>
               <p className='font-medium text-2xl text-primary'><strong className='text-gray-800'>by</strong> {article.author.name}</p>
-              <p className="text-md text-gray-500"> {new Date(article.created_at).toLocaleDateString("en-US", {year: "numeric", month: "long", day: "numeric"})}</p>
-              <p>{article.content}</p>
+              <p className="text-md text-gray-500 mb-4"> {new Date(article.created_at).toLocaleDateString("en-US", {year: "numeric", month: "long", day: "numeric"})}</p>
+              <p className="text-justify text-lg text-gray-700 mb-20 font-medium leading-9">{article.content}</p>
             </li>
           ))}
         </ul>
