ff78575f53289e585ed75b2f8b94417e270e9e26
Husseini <mhmd.husseinyy@gmail.com>
2023-09-15 05:59:42 +0300

    creating navlink component

---
+++ src/components/NavLink/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavLink = ({ to, children, isActive }) => {
+  const linkClasses = `text-lg font-bold  ${
+    isActive ? 'bg-secondary px-4 py-2 text-white rounded-xl' : 'text-secondary hover:text-primary'
+  }`;
+
+  return (
+    <Link to={to} className={linkClasses}>
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
