0427cd92b77143d9885905b91fde48be52950acb
Husseini <mhmd.husseinyy@gmail.com>
2023-09-15 06:00:11 +0300

    styling the active navlink in navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import { Outlet, Link, useNavigate } from 'react-router-dom'; 
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import logo from '../../logo.svg';
 import ButtonSm from '../ButtonSm';
+import NavLink from '../NavLink'; 
 
 const Nav = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const handleSignInClick = () => {
-    navigate('/auth'); 
+    navigate('/auth');
   };
 
   return (
     <div>
-      <div className=" py-8 flex justify-between items-center mx-auto max-w-screen-xl">
+      <div className="py-8 flex justify-between items-center mx-auto max-w-screen-xl">
         <img src={logo} alt="Logo" className="h-22 w-22" />
         <div className="flex justify-between w-1/2">
-          <Link to="/" className="text-secondary hover:text-primary text-lg font-bold hover:underline">Home</Link>
-          <Link to="/houses" className="text-secondary hover:text-primary text-lg font-bold hover:underline">Houses</Link>
-          <Link to="/lands" className="text-secondary hover:text-primary text-lg font-bold hover:underline">Lands</Link>
-          <Link to="/trends" className="text-secondary hover:text-primary text-lg font-bold hover:underline">Trends</Link>
-          <a href="/dashboard" className="text-secondary hover:text-primary text-lg font-bold hover:underline">Dashboard</a>
+          <NavLink to="/" isActive={location.pathname === '/'}>
+            Home
+          </NavLink>
+          <NavLink to="/houses" isActive={location.pathname.startsWith('/houses')}>
+            Houses
+          </NavLink>
+          <NavLink to="/lands" isActive={location.pathname.startsWith('/lands')}>
+            Lands
+          </NavLink>
+          <NavLink to="/trends" isActive={location.pathname.startsWith('/trends')}>
+            Trends
+          </NavLink>
+          <NavLink to="/dashboard" isActive={location.pathname.startsWith('/dashboard')}>
+            Dashboard
+          </NavLink>
         </div>
-        <ButtonSm buttonText='Sign In' onClick={handleSignInClick} /> 
+        <ButtonSm buttonText="Sign In" onClick={handleSignInClick} />
       </div>
       <Outlet />
     </div>
