5f53af336c2f6e86403f5c0da5fd1e964f196605
Husseini <mhmd.husseinyy@gmail.com>
2023-09-15 06:00:32 +0300

    modifying style of property card

--- src/components/PropertyCard/index.jsx
+++ src/components/PropertyCard/index.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 const PropertyCard = ({ property }) => {
   return (
-    <Card key={property.id} className="w-full max-w-[22rem] shadow-lg  hover:text-secondary rounded-lg p-4 mt-10 mb-10 transition-colors duration-100 ease-in-out border-primary border-2">
+    <Card key={property.id} className="w-full max-w-[22rem] shadow-lg  hover:text-secondary rounded-lg p-4 mt-8 mb-8 transition-colors duration-100 ease-in-out border-primary border-2">
       <CardHeader className='mt--6' floated={false} color="blue-gray">
         <img className='rounded-lg'src={`${property.img1}`}alt={property.title} />
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
       </CardHeader>
-      <CardBody>
+      <CardBody className='p-3'>
         <div className="mb-3 flex items-center justify-between">
-          <Typography variant="h5" color="blue-gray" className="flex font-medium transition-colors duration-300 ease-in-out mt-3">
+          <Typography variant="h5" color="blue-gray" className="flex font-medium transition-colors duration-300 ease-in-out ">
             <p>{property.title}</p> <p className='text-gray-500 text-sm'> {property.area} sqft</p>
           </Typography>
         </div>
@@ -20,7 +20,7 @@ const PropertyCard = ({ property }) => {
           <p className='text-primary font-bold'>{property.city.city}</p>          
         </Typography>
       </CardBody>
-      <CardFooter >
+      <CardFooter className='py-0 px-3' >
       <Link to={`/property/${property.id}`}>
         <Button size="lg" fullWidth={true} className=' text-stone-700 p-3 cursor-pointer hover:bg-primary transition-colors duration-100'>
           Show Details
