c7ae80715ebc65f0940c406169ffea84544bca02
Husseini <mhmd.husseinyy@gmail.com>
2023-09-18 06:07:25 +0300

    styling the meetings page

--- src/pages/Meetings/index.jsx
+++ src/pages/Meetings/index.jsx
@@ -15,7 +15,6 @@ const Meetings = () => {
   });
 
   const token = useSelector((state) => state.auth.token);
-
   const handleStartTimeChange = (day, value) => {
     setAvailability({ ...availability, [day]: { ...availability[day], start_time: value } });
   };
@@ -29,7 +28,6 @@ const Meetings = () => {
 
     try {
       const response = await sendRequest({ method: "POST", route: `user/setAvailable`, body: { available_time: availableTime }, token });
-
       console.log(response);
     } catch (error) {
       console.error('Error updating available time:', error);
@@ -37,38 +35,36 @@ const Meetings = () => {
   };
 
   return (
-    <div className='text-lg text-gray-800 font-medium leading-9 max-w-2xl ml-20 '>
-      <h2 className='text-3xl text-gray-800 font-medium leading-9 '>Set your availability</h2>
-      <div className='w-36 h-1.5 bg-gradient-to-r from-primary to-black mb-5 mt-3'></div>
+    <div className="text-lg text-gray-800 font-medium leading-9 max-w-2xl ml-20">
+      <h2 className="text-3xl text-gray-800 font-medium leading-9">Set your availability</h2>
+      <div className="w-36 h-1.5 bg-gradient-to-r from-primary to-black mb-5 mt-3"></div>
       <div>
-        <label className='text-xl text-secondary font-semibold'>Available hours:</label>
+        <label className="text-xl text-secondary font-semibold">Available hours:</label>
         {Object.keys(availability).map((day) => (
-          <div key={day}>
-            {day.charAt(0).toUpperCase() + day.slice(1)}:
-            <select
-              value={availability[day].start_time}
-              onChange={(e) => handleStartTimeChange(day, e.target.value)}
-            >
+          <div key={day} className="mb-4 flex items-center">
+            <p className="w-32">{day.charAt(0).toUpperCase() + day.slice(1)}:</p>
+            <select value={availability[day].start_time} onChange={(e) => handleStartTimeChange(day, e.target.value)} className="w-24">
               {Array.from({ length: 24 }, (_, i) => {
                 const hour = String(i).padStart(2, '0');
-                return <option key={hour} value={`${hour}:00`}>{`${hour}:00`}</option>;
+                return (
+                  <option key={hour} value={`${hour}:00`}>{`${hour}:00`}</option>
+                );
               })}
             </select>
-            <span> — </span>
-            <select
-              value={availability[day].end_time}
-              onChange={(e) => handleEndTimeChange(day, e.target.value)}
-            >
+            <span className="mx-2">—</span>
+            <select value={availability[day].end_time} onChange={(e) => handleEndTimeChange(day, e.target.value)} className="w-24" >
               {Array.from({ length: 24 }, (_, i) => {
                 const hour = String(i).padStart(2, '0');
-                return <option key={hour} value={`${hour}:00`}>{`${hour}:00`}</option>;
+                return (
+                  <option key={hour} value={`${hour}:00`}>{`${hour}:00`}</option>
+                );
               })}
             </select>
           </div>
         ))}
       </div>
-      <div className='mt-6'>
-        <ButtonSm onClick={handleSubmit} buttonText='Save' />
+      <div className="mt-6">
+        <ButtonSm onClick={handleSubmit} buttonText="Save" />
       </div>
     </div>
   );
