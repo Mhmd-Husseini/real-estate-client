3416a9aad91a29916c5bb7d746741e55df047332
Husseini <mhmd.husseinyy@gmail.com>
2023-09-20 18:55:41 +0300

    fixing bugs in sidebar component

--- src/components/SideNav/index.jsx
+++ src/components/SideNav/index.jsx
@@ -12,22 +12,22 @@ const SideNav = () => {
         </div>
         <ul className="mt-4 text-lg text-gray-800 flex flex-col items-start">
           <li className="flex items-center mb-4">
-            <NavLink to="" activeClassName="text-primary">
-              <FontAwesomeIcon icon={faUser} className="mr-3" /> Profile 
+            <NavLink to="" >
+              <FontAwesomeIcon icon={faUser} className="mr-3" /> Profile
             </NavLink>
           </li>
           <li className="flex items-center mb-4">
-            <NavLink to="properties" activeClassName="text-primary">
+            <NavLink to="properties" >
               <FontAwesomeIcon icon={faBuilding} className="mr-3" /> My Properties
             </NavLink>
           </li>
           <li className="flex items-center mb-4">
-            <NavLink to="add" activeClassName="text-primary">
+            <NavLink to="add" >
               <FontAwesomeIcon icon={faHome} className="mr-3" /> Add Property
             </NavLink>
           </li>
           <li className="flex items-center mb-2">
-            <NavLink to="meetings" activeClassName="text-primary">
+            <NavLink to="meetings" >
               <FontAwesomeIcon icon={faCalendar} className="mr-3" /> Meetings
             </NavLink>
           </li>
