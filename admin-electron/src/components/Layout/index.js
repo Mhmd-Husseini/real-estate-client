e297c51416fa2b837024ddeebb069d5ce2d6dd6a
Husseini <mhmd.husseinyy@gmail.com>
2023-09-25 02:55:12 +0300

    admin: adding layout of the dashboard

---
+++ admin-electron/src/components/Layout/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import logo from '../../logo.svg'
+
+const Layout = () => {
+  return (
+    <div className="flex min-h-screen">
+      <div className="bg-primary text-white w-1/6 min-h-screen">
+        <div className="p-4">
+          <div className="text-3xl font-bold my-7 mb-16">Dashboard</div>
+            <ul className="space-y-2 text-xl">
+            <li>
+              <NavLink to="/admin/" className="block text-white hover:bg-secondary px-4 py-2 rounded" activeClassName="bg-secondary">Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to="/admin/users" className="block text-white hover:bg-secondary px-4 py-2 rounded" activeClassName="bg-secondary">Users</NavLink>
+            </li>
+            <li>
+              <NavLink to="/admin/authors" className="block text-white hover:bg-secondary px-4 py-2 rounded" activeClassName="bg-secondary">Authors</NavLink>
+            </li>
+            <li>
+              <NavLink to="/admin/analytics" className="block text-white hover:bg-secondary px-4 py-2 rounded" activeClassName="bg-secondary">Analytics</NavLink>
+            </li>
+            </ul>
+        </div>
+      </div>
+
+      <div className="flex-1 p-4">
+        <div className="mb-4 flex justify-between mx-7">
+          <img src={logo} alt="Logo" className="w-20 h-20"/>
+          <button className="text-lg text-secondary font-semibold mt-2">Logout</button>
+        </div>
+        <div className="mx-7 my-16"><Outlet /></div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
