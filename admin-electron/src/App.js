290083c727e813e4b13d99674e40cf96857fc05a
Husseini <mhmd.husseinyy@gmail.com>
2023-09-24 23:00:42 +0300

    admin: adding login and dashboard routes

--- admin-electron/src/App.js
+++ admin-electron/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+        {/* <Login />
+        <Dashboard />
+        <Route exact path="/" component={Login} />
+        <Route path="/dashboard" component={Dashboard} /> */}
+
