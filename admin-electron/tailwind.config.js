fcb8aafaf8a7a68a14ffe9021e9f8f4d44548893
Husseini <mhmd.husseinyy@gmail.com>
2023-09-24 22:58:52 +0300

    admin: configuring tailwind

---
+++ admin-electron/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8DB78F',    
+        secondary: '#587259',  
+      },
+    },
+  },
+  plugins: [],
+};
