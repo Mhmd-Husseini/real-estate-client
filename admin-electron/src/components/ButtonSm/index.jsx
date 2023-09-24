46e9cacfe64fc00b2473b28e1bd17aab27f0efc6
Husseini <mhmd.husseinyy@gmail.com>
2023-09-24 22:59:51 +0300

    creating button component

---
+++ admin-electron/src/components/ButtonSm/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const ButtonSm = ({ buttonText, onClick }) => {
+  return (
+    <button onClick={() => onClick()} className="text-neutral-800 py-4 font-semibold px-5 rounded-tr-2xl bg-primary hover:bg-secondary hover:text-white text-sm whitespace-pre-wrap">
+        {buttonText}   <FontAwesomeIcon icon={faArrowRight} />
+    </button>
+  )
+}
+
+export default ButtonSm
