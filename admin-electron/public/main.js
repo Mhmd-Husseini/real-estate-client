40688db1dd80b437c2d9346eb4fd2f750014b453
Husseini <mhmd.husseinyy@gmail.com>
2023-09-25 18:34:09 +0300

    admin: increasing app window size

--- admin-electron/public/main.js
+++ admin-electron/public/main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1100,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }
