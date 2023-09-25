85c2d415910f529c8a2671a75da45d99d09723ef
Husseini <mhmd.husseinyy@gmail.com>
2023-09-25 18:34:32 +0300

    admin: adding route for users page

--- admin-electron/src/App.js
+++ admin-electron/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import Users from './pages/Users';
 import Layout from './components/Layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -11,8 +12,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='dashboard/' element={<Layout />}>
           <Route index element={<Profile />} />
-          {/* <Route path='users' element={<Users />} />  */}
-          {/* <Route path='users' element={<Authors />} />  */}
+          <Route path='users' element={<Users />} /> 
           {/* <Route path='properties' element={<Properties />} />
           <Route path='analytics' element={<Analytics />} /> */}
         </Route>
