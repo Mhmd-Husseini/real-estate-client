6975869bc60e17a7f6ac25a0e389c15584c28c51
Husseini <mhmd.husseinyy@gmail.com>
2023-09-26 01:43:19 +0300

    admin: adding search functionality in users page

--- admin-electron/src/pages/Users/index.js
+++ admin-electron/src/pages/Users/index.js
@@ -10,7 +10,9 @@ const TABLE_HEAD = ["ID", "Name", "Email", "Phone", "User Type", "Delete"];
 function Users() {
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const [showModal, setShowModal] = useState(false); 
+  const [searchQuery, setSearchQuery] = useState('');
 
   const fetchUsers = async () => {
     try {
@@ -27,6 +29,7 @@ function Users() {
         }));
 
         setUsers(mappedUsers);
+        setFilteredUsers(mappedUsers); 
       } else {
         console.error("Error fetching user data");
       }
@@ -39,6 +42,20 @@ function Users() {
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  useEffect(() => {
+    const filtered = users.filter((user) => {
+      const query = searchQuery ? searchQuery.toLowerCase() : ''; 
+      return (
+        (user.name && user.name.toLowerCase().includes(query)) ||
+        (user.email && user.email.toLowerCase().includes(query)) ||
+        (user.phone && user.phone.toLowerCase().includes(query)) ||
+        (user.userType && user.userType.toLowerCase().includes(query))
+      );
+    });
+    setFilteredUsers(filtered);
+  }, [searchQuery, users]);
+  
 
   const deleteUser = async (id) => {
     try {
@@ -64,12 +81,17 @@ function Users() {
       <CardHeader floated={false} shadow={false} className="rounded-none">
         <div className="mb-4 flex flex-col justify-between gap-8 md:flex-row md:items-center">
           <div className='mb-10'>
-            <h2 className="text-3xl text-gray-800 font-medium leading-9">Analytics and Statistics </h2>
+            <h2 className="text-3xl text-gray-800 font-medium leading-9">User Management </h2>
             <div className="w-36 h-1.5 bg-gradient-to-r from-primary to-black mt-3"></div>
           </div>
           <div className="flex w-full shrink-0 gap-2 md:w-max">
             <div className="w-full md:w-72">
-              <Input label="Search" />
+              <Input
+                label="Search"
+                placeholder="Search by name, email, phone, or user type"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
             </div>
           </div>
         </div>
@@ -88,8 +110,8 @@ function Users() {
             </tr>
           </thead>
           <tbody>
-            {users.map(({ id, name, email, phone, userType }, index) => {
-              const isLast = index === users.length - 1;
+            {filteredUsers.map(({ id, name, email, phone, userType }, index) => {
+              const isLast = index === filteredUsers.length - 1;
               const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
               return (
@@ -134,7 +156,7 @@ function Users() {
           </tbody>
         </table>
       </CardBody>
-      {showModal && (<Modal message="User Deleted Successfuly" onClose={closeModal} />)} 
+      {showModal && (<Modal message="User Deleted Successfully" onClose={closeModal} />)} 
     </Card>
   );
 }
