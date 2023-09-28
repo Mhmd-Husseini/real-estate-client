b4788424999015ac3872d603ab80fca8a6e92764
Husseini <mhmd.husseinyy@gmail.com>
2023-09-28 18:06:19 +0300

    admin: fixing styles of analytics page

--- admin-electron/src/pages/Analytics/index.js
+++ admin-electron/src/pages/Analytics/index.js
@@ -22,7 +22,7 @@ const AnalyticsPage = () => {
   }, []);
 
   if (!analyticsData) {
-    return <div>Loading...</div>;
+    return <div>...</div>;
   }
 
   const pieChartData = {
@@ -30,11 +30,11 @@ const AnalyticsPage = () => {
     datasets: [
       {
         data: [analyticsData.usersCount, analyticsData.authorsCount],
-        backgroundColor: ['#FF5733', '#33FF57'],
+        backgroundColor: ['#8DB78F', '#587259'],
       },
     ],
   };
-
+  
   const barChartData = {
     labels: analyticsData.propertyCountsByCity.map((data) => data.city_name),
     datasets: [
@@ -52,7 +52,7 @@ const AnalyticsPage = () => {
       {
         label: 'Meeting Count',
         data: analyticsData.meetingCounts.map((data) => data.count),
-        borderColor: '#FF5733',
+        borderColor: '#8DB78F',
         fill: false,
       },
     ],
@@ -76,18 +76,18 @@ const AnalyticsPage = () => {
       {
         label: 'Land Avg Price',
         data: analyticsData.landAvgPrices.map((data) => data.avg_price_per_100m2),
-        backgroundColor: '#FF5733',
+        backgroundColor: '#587259',
       },
     ],
   };
-
+ 
   const homeAvgPricesData = {
     labels: analyticsData.homeAvgPrices.map((data) => data.city_name),
     datasets: [
       {
         label: 'Home Avg Price',
         data: analyticsData.homeAvgPrices.map((data) => data.avg_price_per_100m2),
-        backgroundColor: '#33FF57',
+        backgroundColor: '#8DB78F',
       },
     ],
   };
@@ -102,7 +102,7 @@ const AnalyticsPage = () => {
     datasets: [
       {
         data: propertyCountsByCityData.map((data) => data.value),
-        backgroundColor: ['#FF5733', '#33FF57', '#3366FF', '#FF33CC', '#33CCFF', '#33FF33', '#FF9933'],
+        backgroundColor: ['#8DB78F','#66CDAA', '#33FF57', '#48D1CC', '#2E8B57', '#6B8E23', '#3CB371', '#98FB98'],
       },
     ],
   };
@@ -113,34 +113,34 @@ const AnalyticsPage = () => {
         <h2 className="text-3xl text-gray-800 font-medium leading-9">Analytics and Statistics </h2>
         <div className="w-36 h-1.5 bg-gradient-to-r from-primary to-black mt-3"></div>
       </div>
-      <div className="mb-20 mt-12 w-full max-w-4xl">
-        <h2 className="text-xl font-semibold mb-2">Properties by City</h2>
+      <div className="mb-20 mt-12 w-full max-w-2xl">
+        <h2 className="text-xl font-bold text-secondary mb-2">Properties by City:</h2>
         <Bar data={barChartData} />
       </div>
       <div className='flex w-full'>
-        <div className="mb-6 flex-1 max-w-md">
-          <h2 className="text-xl font-semibold mb-5">User Types</h2>
+        <div className="mb-6 flex-1 max-w-[26em]">
+          <h2 className="text-xl font-bold text-secondary mb-5">User Types:</h2>
           <Pie data={pieChartData} />
         </div>
-        <div className="mb-20 flex-1 max-w-md">
-          <h2 className="text-xl font-semibold mb-5 ">Number of Properties in Cities</h2>
+        <div className="mb-20 flex-1 max-w-[26em]">
+          <h2 className="text-xl font-bold text-secondary mb-5 ">Number of Properties in Cities:</h2>
           <Pie data={pieChartData2} />
         </div>
       </div>
-      <div className="mb-20 w-full max-w-4xl">
-        <h2 className="text-xl font-semibold mb-2">Weekly Meeting Counts</h2>
+      <div className="mb-20 w-full max-w-2xl">
+        <h2 className="text-xl font-bold text-secondary mb-2">Meetings Booked: <span className='text-sm text-primary'>per week</span></h2>
         <Line data={lineChartData} />
       </div>
-      <div className="mb-20 w-full max-w-4xl">
-        <h2 className="text-xl font-semibold mb-2">Users Count by Week</h2>
+      <div className="mb-20 w-full max-w-2xl">
+        <h2 className="text-xl font-bold text-secondary mb-2">Users Registirations: <span className='text-sm text-primary'>per week</span></h2>
         <Line data={usersCountByWeekData} />
       </div>
-      <div className="mb-20 w-full max-w-4xl">
-        <h2 className="text-xl font-semibold mb-2">Land Average Prices per 100m2</h2>
+      <div className="mb-20 w-full max-w-2xl">
+        <h2 className="text-xl font-bold text-secondary mb-2">Land Average Prices: <span className='text-sm text-primary'>per 100m2</span></h2>
         <Bar data={landAvgPricesData} />
       </div>
-      <div className="mb-20 w-full max-w-4xl">
-        <h2 className="text-xl font-semibold">Home Average Prices per 100m2</h2>
+      <div className="mb-20 w-full max-w-2xl">
+        <h2 className="text-xl font-bold text-secondary">Home Average Prices: <span className='text-sm text-primary'>per 100m2</span></h2>
         <Bar data={homeAvgPricesData} />
       </div>
     </div>
