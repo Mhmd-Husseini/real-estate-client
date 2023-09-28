efe8a8c92cb00cbd9cf5e196b1434f67870e5a3b
Husseini <mhmd.husseinyy@gmail.com>
2023-09-28 18:05:47 +0300

    admin: changing the name of main.js to electron.js

---
+++ admin-electron/public/electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1300,
+    height: 900,
+    title: 'Lebanon RealEstate Insights',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.removeMenu();
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
