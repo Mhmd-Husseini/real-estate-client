b1484fb7104ce4b3703c25a6d2371edfdac64b5b
Husseini <mhmd.husseinyy@gmail.com>
2023-09-30 17:56:08 +0300

    updating button component to render conditionally

--- client/src/components/ButtonSm/index.jsx
+++ client/src/components/ButtonSm/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const ButtonSm = ({ buttonText, onClick }) => {
   return (
     <button onClick={() => onClick()} className="text-neutral-800 py-4 font-semibold px-5 rounded-tr-2xl bg-primary hover:bg-secondary hover:text-white text-sm whitespace-pre-wrap">
-        {buttonText}   <FontAwesomeIcon icon={faArrowRight} />
+        {buttonText}   {buttonText==="Logout"? <FontAwesomeIcon icon={faSignOutAlt} className="mr-3"  />: <FontAwesomeIcon icon={faArrowRight} />}
     </button>
   )
 }
