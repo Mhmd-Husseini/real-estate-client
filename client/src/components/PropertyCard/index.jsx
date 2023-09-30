7101b9551be07cae98908f4f755ada304021b014
Husseini <mhmd.husseinyy@gmail.com>
2023-09-30 19:43:57 +0300

    modifying style of property card component

--- client/src/components/PropertyCard/index.jsx
+++ client/src/components/PropertyCard/index.jsx
@@ -3,7 +3,10 @@ import { Card, CardHeader, CardBody, CardFooter, Typography, Button } from '@mat
 import { Link } from 'react-router-dom'; 
 
 const PropertyCard = ({ property }) => {
-  return (
+    const price = parseInt(property.price);
+    const formattedPrice = price.toString().replace(/\d(?=(\d{3})+$)/g, '$&,');  
+
+    return (
     <Card key={property.id} className="w-full max-w-[22rem] shadow-lg  hover:text-secondary rounded-lg p-4 mt-8 mb-8 transition-colors duration-100 ease-in-out border-primary border-2">
       <CardHeader className='mt--6' floated={false} color="blue-gray">
         <img className='rounded-lg'src={`${property.img1}`}alt={property.title} />
@@ -12,12 +15,14 @@ const PropertyCard = ({ property }) => {
       <CardBody className='p-3'>
         <div className="mb-3 flex items-center justify-between">
           <Typography variant="h5" color="blue-gray" className="flex font-medium transition-colors duration-300 ease-in-out ">
-            <span>{property.title}</span> <span className='text-gray-500 text-sm'> {property.area} sqft</span>
+            <span>{property.title}</span> 
           </Typography>
         </div>
         <Typography className=" text-secondary font-semibold">
-          {property.price}$ <br></br>
-          <span className='text-primary font-bold'>{property.city.city}</span>          
+          $ {formattedPrice}  <br></br>
+          <div className='flex justify-between'>
+          <span className='text-primary font-bold'>{property.city.city}</span>   <span className='text-primary font-bold text-sm'> {property.area} sqft</span>
+          </div>
         </Typography>
       </CardBody>
       <CardFooter className='py-0 px-3' >
